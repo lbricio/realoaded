f58f59518adcea1723a1a027f5eb4dcc1e8429cb
lbricio <kirowhisky@gmail.com>
2022-08-28 17:34:58 -0300

    v2

---
+++ ft_sort_params_v2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		ft_putchar(str[i++]);
+	ft_putchar('\n');
+}
+
+int	ft_strcmp(char *s1, char signal, char *s2)
+{
+	while (*s1 == *s2 && *s1++ && *s2++)
+		;
+	if ((signal == '>' && *s1 > *s2)
+		|| (signal == '<' && *s1 < *s2))
+		return (1);
+	return (0);
+}
+
+void ft_swap(char **s1, char **s2)
+{
+	char *strtmp;
+
+	strtmp = *s1;
+	*s1 = *s2;
+	*s2 = strtmp;
+}
+
+void	ft_sort(int argc, char **argv)
+{
+	int	ascend;
+	int descend;
+
+	ascend = 1;
+	while (ascend < argc)
+	{
+		descend = argc - 1;
+		while (ascend < descend)
+		{
+			if (ft_strcmp(argv[ascend], '>', argv[descend]))
+				ft_swap(&argv[ascend], &argv[descend]);
+			descend--;
+		}
+		ascend++;
+	}
+}
+
+int	main(int argc, char **argv)
+{	
+	int i;
+
+	ft_sort(argc, argv);
+	i = 1;
+	while(i < argc)
+	{
+		ft_putstr(argv[i]);
+		i++;
+	}
+	return (0);
+}
